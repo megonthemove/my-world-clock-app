964b0c06ccc812760bb6577ec940d191ca788a86
megonthemove <meganlinney@pm.me>
2023-09-19 19:14:47 -0600

    started over. attached github link

--- src/java.js
+++
@@ -1,9 +0,0 @@
-function showCity(event) {
-  if (event.target.value.length > 0) {
-  let currentTime = moment().tz(event.target.value).format("dddd, MMMM D, YYYY h:mm P");
- 
-  alert(`It is ${currentTime} in ${event.target.value}`);
-}
-}
-let selectCity = document.querySelector("#city");
-selectCity.addEventListener("change", showCity);
